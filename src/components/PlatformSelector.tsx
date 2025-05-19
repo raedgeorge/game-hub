707cfef12faf24e80290e1ec2f771d39b217c23b
raed <raedgeorge2014@gmail.com>
2025-05-19 22:51:22 +0300

    Refactor-create lookup hooks

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,6 +1,7 @@
 import { BsChevronDown } from "react-icons/bs";
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import usePlatforms from "../hooks/usePlatforms";
+import usePlatform from "../hooks/usePlatform";
 
 interface Props {
   onSelectPlatform: (platformId: number) => void;
@@ -9,16 +10,15 @@ interface Props {
 
 const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
   const { data, error } = usePlatforms();
-  const selectedPlatform = data?.results.find(
-    (p) => p.id === selectedPlatformId
-  );
+
+  const platform = usePlatform(selectedPlatformId);
 
   if (error) return null;
 
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.name || "Platforms"}
+        {platform?.name || "Platforms"}
       </MenuButton>
       <MenuList>
         {data?.results.map((platform) => (
---
+++ src/hooks/useGenre.ts
@@ -0,0 +1,9 @@
+import useGenres from "./useGenres";
+
+const useGenre = (id?: number) => {
+  const { data } = useGenres();
+
+  return data?.results.find((genre) => genre.id === id);
+};
+
+export default useGenre;
